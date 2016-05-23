6dc22b9a41f11890b740d293954b5efd7fc1b3dc
strivemag <talga.tawit@gmail.com>
2016-05-24 01:19:53 +0600

    e is ok

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,16 @@
 class CommentsController < ApplicationController
-	befor_action :autheticate_user!
-	
+	before_action :authenticate_user!
+
   def create
   	@post = Post.find(params[:post_id])
   	@comment = Comment.create(params[:comment].permit(:content))
   	@comment.user_id = current_user.id
   	@comment.post_id = @post.id
+
   	if @comment.save
-  		redirect_to post_patg(@post)
+  		redirect_to post_path(@post)
   	else
   		render 'new'
   	end
-
-
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 	class PostsController < ApplicationController
-	before_action :find_post, only: [:show, :edit, :update, :destroy]
-	before_action :authenticate_user!, except: [:index, :show]
+	  before_action :find_post, only: [:show, :edit, :update, :destroy]
+	  before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@posts = Post.all.order("created_at DESC")
 	end
 
 	def show
-		@comments = Comment.where(post_id: @pos)
+		@comments = Comment.where(post_id: @post)
 	end
 
 	def new
